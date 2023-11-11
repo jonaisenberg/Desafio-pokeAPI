e3ac572c0882b49f9c25458f51bc28bd448c1b5f
jonaisenberg <jonathan.araos.a@gmail.com>
2023-11-11 03:16:57 -0300

    my first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div className='inicio'>
+        <h1>Bienvenido maestro Pokemón</h1>
+        <img className='pikachu' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg' alt='pokemon Pikachu' />
+      </div>
+    </>
+  )
+}
+export default Home
